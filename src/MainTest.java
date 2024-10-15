030cad027103d8d15977d32427c2041b2ee60243
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-10-15 11:12:57 -0700

    Add testing skeleton and support files

---
+++ src/MainTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testParseCSV() {
+      // You will finish implementing this method in Wave 3
+      // TODO: Call parseCSV("data/small_commit_data.csv") here and set it to an actual variable
+
+
+      // Sets up the expected value for you. You do not need to edit this part
+      List<Map<String, String>> expectedCommits = new ArrayList<>();
+
+      Map<String, String> commit1 = new HashMap<>();
+      commit1.put("chg", "179");
+      commit1.put("tm", "2024-10-15T08:02:04Z");
+      commit1.put("id", "fork1");
+      expectedCommits.add(commit1);
+
+      Map<String, String> commit2 = new HashMap<>();
+      commit2.put("chg", "1");
+      commit2.put("tm", "2024-10-12T19:48:59Z");
+      commit2.put("id", "fork2");
+      expectedCommits.add(commit2);
+
+      Map<String, String> commit3 = new HashMap<>();
+      commit3.put("chg", "13");
+      commit3.put("tm", "2024-10-10T21:56:18Z");
+      commit3.put("id", "fork2");
+      expectedCommits.add(commit3);
+      // Finish expected value setup
+
+      // TODO: Assert that the expected equals the actual
+    }
+}
